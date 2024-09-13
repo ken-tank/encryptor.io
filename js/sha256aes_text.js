3d2648c59ada31e36770acbcb2835ab9746baab9
KenTank <ulilalkhaq4@gmail.com>
2024-09-13 22:15:51 +0700

    v2

--- js/sha256aes_text.js
+++ js/sha256aes_text.js
@@ -1,7 +1,7 @@
 function SHA256AES_Text() 
 {
     var key = document.getElementById("sha256aes_key");
-    var iv = document.getElementById("sha256aes_iv");
+    var iv = "SV85x1MH5OM4qwtt/rlllaNbmjXNtCfiiFA3QzpSggE=";
     var input = document.getElementById("sha256aes_input");
     var output = document.getElementById("sha256aes_output");
     var encrypt = document.getElementById("sha256aes_encrypt");
